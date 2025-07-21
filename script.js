b213dd1bd11e33d52df89dcb627f371ea3f36306
Dharani-shree <shreedharani21@gmail.com>
2025-07-21 15:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function add()
+{
+    var num1=parseInt(document.getElementById("input1").value)
+   var num2=parseInt(document.getElementById("input2").value)
+   console.log(num1+num2)
+}
+function subtract()
+{
+    var num1=parseInt(document.getElementById("input1").value)
+   var num2=parseInt(document.getElementById("input2").value)
+   console.log(num1-num2)
+}
+function multiply()
+{
+    var num1=parseInt(document.getElementById("input1").value)
+   var num2=parseInt(document.getElementById("input2").value)
+   console.log(num1*num2)
+}
+function division()
+{
+    var num1=parseInt(document.getElementById("input1").value)
+   var num2=parseInt(document.getElementById("input2").value)
+   console.log(num1/num2)
+}
